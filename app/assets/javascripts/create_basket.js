b212b59602a3449947684ec4590e426c98a472a3
Mike Stone <mikestone14@gmail.com>
2014-03-03 12:33:44 -0500

    Added bootstrap javascript and basic function for adding Ingredient types

--- app/assets/javascripts/create_basket.js
+++ app/assets/javascripts/create_basket.js
@@ -9,6 +9,7 @@ RecipeMe.buildBasket = function() {
 	})
 	.done(function(data) {
 		console.log("success");
+		debugger;
 		RecipeMe.displayIngredients(data);
 	})
 	.fail(function(data) {
@@ -23,18 +24,46 @@ RecipeMe.buildBasket = function() {
 
 
 RecipeMe.displayIngredients = function(ingredients_list) {
-	var $nav_tabs = $('<ul class="nav nav-tabs">'),
-			$protein_tab = $('<li><a href="#protein" data-toggle="tab">protein</a></li>'),
+	var $nav_tabs = $('<ul class="nav nav-tabs" id="myTab">'),
+			$protein_tab = $('<li class="active"><a href="#protein" data-toggle="tab">protein</a></li>'),
 			$vegetable_tab = $('<li><a href="#vegetable" data-toggle="tab">Vegetable</a></li>'),
 			$sauce_tab = $('<li><a href="#sauce" data-toggle="tab">Sauce</a></li>'),
 			$spice_tab = $('<li><a href="#spice" data-toggle="tab">Spice</a></li>'),
 			$dairy_tab = $('<li><a href="#dairy" data-toggle="tab">Dairy</a></li>'),
 			$starch_tab = $('<li><a href="#starch" data-toggle="tab">Starch</a></li>'),
-			$content = $('#content');
+			$content = $('#content'),
+			$tab_content = $('<div class="tab-content" id="myTabContent">'),
+			$protein_pane = $('<div class="tab-pane" id="protein"></div>');
+			$vegetable_pane = $('<div class="tab-pane" id="vegetable"></div>');
+			$sauce_pane = $('<div class="tab-pane" id="sauce"></div>');
+			$spice_pane = $('<div class="tab-pane" id="spice"></div>');
+			$dairy_pane = $('<div class="tab-pane" id="dairy"></div>');
+			$starch_pane = $('<div class="tab-pane" id="starch"></div>'),
+			$ingred_div = $('<div class="col-md-8" id="ingred-div">'),
+			i = 0,
+			l = ingredients_list.length;
+
 
 	$content.text("");
 
 
+	for(; i < l; i++) {
+		var ingredient = ingredients_list[i];
+		// if (ingredient.ingred_type === "protein") {
+		// 	$protein_pane.append(ingredient.name);
+		// } else if (ingredient.ingred_type === "vegetable"){
+		// 	$vegetable_pane.append(ingredient.name);
+		// } else if (ingredient.ingred_type === "sauce") {
+		// 	$sauce_pane.append(ingredient.name);
+		// } else
+
+		$("#" + ingredient.ingred_type).append($('<div>' + ingredient.name + '</div>'));
+
+	};
+	$tab_content.append($protein_pane, $vegetable_pane, $sauce_pane, $spice_pane, $dairy_pane, $starch_pane);
+	$nav_tabs.append($protein_tab, $vegetable_tab, $sauce_tab, $spice_tab, $dairy_tab, $starch_tab);
+	$ingred_div.append($nav_tabs, $tab_content);
+	$content.append($ingred_div);
 
 
 
