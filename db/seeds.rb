8e40d2e432ee064598c357a7c5d03a3b8d584a89
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-03-03 10:18:30 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,109 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Ingredient.create(ingred_type: "protein", name: "steak")
+Ingredient.create(ingred_type: "protein", name: "chicker")
+Ingredient.create(ingred_type: "protein", name: "pork")
+Ingredient.create(ingred_type: "protein", name: "beef")
+Ingredient.create(ingred_type: "protein", name: "sausage")
+Ingredient.create(ingred_type: "protein", name: "turkey")
+Ingredient.create(ingred_type: "protein", name: "egg")
+Ingredient.create(ingred_type: "protein", name: "tuna")
+Ingredient.create(ingred_type: "protein", name: "salmon")
+Ingredient.create(ingred_type: "protein", name: "shrimp")
+Ingredient.create(ingred_type: "protein", name: "tofu")
+Ingredient.create(ingred_type: "protein", name: "bacon")
+Ingredient.create(ingred_type: "protein", name: "tilapia")
+
+
+
+Ingredient.create(ingred_type: "vegetable", name: "celery")
+Ingredient.create(ingred_type: "vegetable", name: "onion")
+Ingredient.create(ingred_type: "vegetable", name: "garlic")
+Ingredient.create(ingred_type: "vegetable", name: "carrot")
+Ingredient.create(ingred_type: "vegetable", name: "tomato")
+Ingredient.create(ingred_type: "vegetable", name: "bell pepper")
+Ingredient.create(ingred_type: "vegetable", name: "spinach")
+Ingredient.create(ingred_type: "vegetable", name: "zuccini")
+Ingredient.create(ingred_type: "vegetable", name: "mushrooms")
+Ingredient.create(ingred_type: "vegetable", name: "lettuce")
+Ingredient.create(ingred_type: "vegetable", name: "corn")
+Ingredient.create(ingred_type: "vegetable", name: "cabbage")
+Ingredient.create(ingred_type: "vegetable", name: "eggplant")
+Ingredient.create(ingred_type: "vegetable", name: "kale")
+Ingredient.create(ingred_type: "vegetable", name: "cucumber")
+Ingredient.create(ingred_type: "vegetable", name: "pumpkin")
+Ingredient.create(ingred_type: "vegetable", name: "ginger")
+Ingredient.create(ingred_type: "vegetable", name: "peas")
+Ingredient.create(ingred_type: "vegetable", name: "asparagus")
+Ingredient.create(ingred_type: "vegetable", name: "avocado")
+
+Ingredient.create(ingred_type: "sauce", name: "olive oil")
+Ingredient.create(ingred_type: "sauce", name: "vegetable il")
+Ingredient.create(ingred_type: "sauce", name: "water")
+Ingredient.create(ingred_type: "sauce", name: "red wine vinegar")
+Ingredient.create(ingred_type: "sauce", name: "barbeque sauce")
+Ingredient.create(ingred_type: "sauce", name: "balsamic vinegar")
+Ingredient.create(ingred_type: "sauce", name: "tomato sauce")
+Ingredient.create(ingred_type: "sauce", name: "tomato paste")
+Ingredient.create(ingred_type: "sauce", name: "chicken broth")
+Ingredient.create(ingred_type: "sauce", name: "hot sauce")
+Ingredient.create(ingred_type: "sauce", name: "peanut butter")
+Ingredient.create(ingred_type: "sauce", name: "sofrito")
+Ingredient.create(ingred_type: "sauce", name: "soy sauce")
+Ingredient.create(ingred_type: "sauce", name: "beef broth")
+Ingredient.create(ingred_type: "sauce", name: "fish sauce")
+Ingredient.create(ingred_type: "sauce", name: "white wine")
+Ingredient.create(ingred_type: "sauce", name: "red wine")
+
+Ingredient.create(ingred_type: "spice", name: "salt")
+Ingredient.create(ingred_type: "spice", name: "pepper")
+Ingredient.create(ingred_type: "spice", name: "sugar")
+Ingredient.create(ingred_type: "spice", name: "thyme")
+Ingredient.create(ingred_type: "spice", name: "bay leaves")
+Ingredient.create(ingred_type: "spice", name: "red pepper flakes")
+Ingredient.create(ingred_type: "spice", name: "rosemary")
+Ingredient.create(ingred_type: "spice", name: "parsely")
+Ingredient.create(ingred_type: "spice", name: "oregano")
+Ingredient.create(ingred_type: "spice", name: "basil")
+Ingredient.create(ingred_type: "spice", name: "cumin")
+Ingredient.create(ingred_type: "spice", name: "chili powder")
+Ingredient.create(ingred_type: "spice", name: "cinnamon")
+Ingredient.create(ingred_type: "spice", name: "cliantro")
+Ingredient.create(ingred_type: "spice", name: "coriander")
+Ingredient.create(ingred_type: "spice", name: "curry powder")
+Ingredient.create(ingred_type: "spice", name: "nutmeg")
+Ingredient.create(ingred_type: "spice", name: "clove")
+
+Ingredient.create(ingred_type: "starches", name: "pasta")
+Ingredient.create(ingred_type: "starches", name: "brown rice")
+Ingredient.create(ingred_type: "starches", name: "white rice")
+Ingredient.create(ingred_type: "starches", name: "bread")
+Ingredient.create(ingred_type: "starches", name: "quinoa")
+Ingredient.create(ingred_type: "starches", name: "beans")
+Ingredient.create(ingred_type: "starches", name: "noodles")
+Ingredient.create(ingred_type: "starches", name: "potatos")
+
+Ingredient.create(ingred_type: "dairy", name: "parmesan")
+Ingredient.create(ingred_type: "dairy", name: "cheddar")
+Ingredient.create(ingred_type: "dairy", name: "mozzarella")
+Ingredient.create(ingred_type: "dairy", name: "feta")
+Ingredient.create(ingred_type: "dairy", name: "swiss")
+Ingredient.create(ingred_type: "dairy", name: "cheddar jack")
+Ingredient.create(ingred_type: "dairy", name: "cottage cheese")
+Ingredient.create(ingred_type: "dairy", name: "milk")
+Ingredient.create(ingred_type: "dairy", name: "butter")
+Ingredient.create(ingred_type: "dairy", name: "cream")
+Ingredient.create(ingred_type: "dairy", name: "whipped cream")
+Ingredient.create(ingred_type: "dairy", name: "yogurt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
