47d5084c4f2d0008c5a9bbe2db23b32d7ca91c93
Janice Smith <janice.k.n.smith@gmail.com>
2014-03-03 12:01:04 -0500

    modified the order of funtions being called

--- app/assets/javascripts/recipe_list.js
+++ app/assets/javascripts/recipe_list.js
@@ -5,19 +5,21 @@
 var RecipeMe = RecipeMe || {};
 
 // creates the div #recipes-index-content
-RecipeMe.set_up_recipe_ccontainer = function() {
+RecipeMe.set_up_recipe_container = function() {
   recipe_container_element = '<div id="recipes-index-content">';
+  $('#content').append(recipe_container_element);
 };
 
 
 // parses out the ingredients
-RecipeMe.list_ingredients = function(basket) {
+RecipeMe.list_of_ingredients_for_search = function(basket) {
+  ingredient_list = basket.ingredients
+  RecipeMe.request_recipies(ingredient_list)
 };
 
 //Runs set_up_div_container and make API call for the recipies
-RecipeMe.request_recipies = function(event, basket) {
-  RecipeMe.set_up_recipe_ccontainer();
-
+RecipeMe.request_recipies = function(ingredent_list_from_basket) {
+  RecipeMe.set_up_recipe_container();
   var sorted_recipes_array = [],
       app_key = ENV["APP_KEY"],
       app_id = ENV["APP_ID"],
@@ -54,10 +56,6 @@ RecipeMe.sort_recipe_score = function(data) {
 
 // iterates and runs over the array to render
 RecipeMe.render_all = function(recipes) {
-  $('#recipes-index-content').empty();
-  var number_recipes = recipes.length, i;
-  for(i = 0; i < number_recipes; i++) {
-    this.render_one(recipes[i], i);
   }
 };
 
