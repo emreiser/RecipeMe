69bcd65d2339547908a9a7fe5605d03c38b4f1f1
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-03-04 15:13:40 -0500

    write the js test for the welcome page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,13 +9,6 @@ Capybara.javascript_driver = :poltergeist
 
 SimpleCov.start 'rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
