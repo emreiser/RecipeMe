dff6ebafd2acbdcbaf38898425a62a7673c9ef39
Janice Smith <janice.k.n.smith@gmail.com>
2014-03-05 11:50:14 -0500

    setting up for jasmine tests and updating model specs to reflect the new data model

---
+++ spec/javascripts/create_basketsSpec.js
@@ -0,0 +1,8 @@
+// describe("createBasket", function() {
+
+//   it("should make an AJAX request to the correct URL", function() {
+//     RecipeMe.createBasket = function();
+//     spyOn($, "ajax");
+//     expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("/baskets");
+//   });
+// });
