7acc1ef5f69717412d5a2f694a02f3f90423cd4a
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-03-05 15:51:01 -0500

    can delete flash message without refreshing the page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,8 +5,9 @@ RecipeMe.buildHomepage = function() {
 		$image_div = $('<div class="img-bg">'),
 		$inner_img_content = $('<div class="boxy col-md-6 col-md-offset-3 img-content">'),
 		$content_title = $('<h1>Welcome To RecipeMe</h1>'),
-		$content_subtitle = $("<h2>We'll help you find recipes with the ingredients you have</h2>");
-		$content_btn = $("<button class='btn btn-primary btn-lg' id='enter-ingreds'>Enter Ingredients</button>")
+		$content_subtitle = $("<h2>We'll help you find recipes with the ingredients you have</h2>"),
+		$content_btn = $("<button class='btn btn-primary btn-lg' id='enter-ingreds'>Enter Ingredients</button>"),
+		$flash = $('.flash');
 
 	$content_div.text("");
 	$inner_img_content.append($content_title, $content_subtitle, $content_btn);
@@ -14,5 +15,6 @@ RecipeMe.buildHomepage = function() {
 	$content_div.append($image_div);
 	$content_btn.click(function(event) {
 		RecipeMe.getIngredients();
+		$flash.text("");
 	});
 };
