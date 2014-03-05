306e44c301cd34efa8a900c9645c7f6197a28639
Mike Stone <mikestone14@gmail.com>
2014-03-05 18:13:42 -0500

    Changed routing type to avoid duplicate get requests

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ RecipeMe::Application.routes.draw do
   resources :yummly, only: :index
 
   resources :recipes, only: [:index, :create, :show]
-  get '/recipes/:id', to: 'recipes#favorite', as: 'favorite_recipe'
+  post '/recipes/:id', to: 'recipes#favorite', as: 'favorite_recipe'
 end
