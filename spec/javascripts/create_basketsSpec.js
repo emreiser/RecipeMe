1950fb3966d90dec24cfb9426d7bd0d5971259ea
Janice Smith <janice.k.n.smith@gmail.com>
2014-03-05 14:08:33 -0500

    going to use rspec instead

--- spec/javascripts/create_basketsSpec.js
+++
@@ -1,8 +0,0 @@
-// describe("createBasket", function() {
-
-//   it("should make an AJAX request to the correct URL", function() {
-//     RecipeMe.createBasket = function();
-//     spyOn($, "ajax");
-//     expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("/baskets");
-//   });
-// });
